98a17b7e0c11b4f87b927f6eeaf8cd6fe5c3a319
sgknoldus <124128952+sgknoldus@users.noreply.github.com>
2024-01-23 16:44:06 +0530

    Update apps-deployment-script.sh

--- apps-deployment-script.sh
+++ apps-deployment-script.sh
@@ -51,7 +51,7 @@ do
     cd common || exit
     mvn -B package --file pom.xml
     #mvn deploy -s $GITHUB_WORKSPACE/settings.xml
-    mvn deploy -X
+    mvn deploy -X -s $GITHUB_WORKSPACE/settings.xml
     cd ..;;
 
   # case 2 build and deploy order-service
